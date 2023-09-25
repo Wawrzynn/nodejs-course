6258a698d10f03e87accab44b5a7f101a32979c5
Wawrzynn <jedrzejkosakowski@gmail.com>
2023-09-25 13:06:28 +0200

    Project 6 added routing and body parser.

--- Project6/app.js
+++ Project6/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use("/add", (req, res, next) => {
-    console.log("In add middleware!");
-    res.send("<h1>The add page</h1>");
-  });
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  console.log("In middleware!");
-  res.send("<h1>Hello from Express!</h1>");
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
