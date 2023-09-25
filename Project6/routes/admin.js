011d418368f66f61ee7263431c30b802c259f458
Wawrzynn <jedrzejkosakowski@gmail.com>
2023-09-25 17:05:55 +0200

    Project 6 added static files and routing with path.

--- Project6/routes/admin.js
+++ Project6/routes/admin.js
@@ -1,11 +1,10 @@
 const express = require("express");
-
+const path = require("path");
 const router = express.Router();
+const rootDir = require("../util/path");
 
-router.get("/add", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type'submit>Add</button></form>"
-  );
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
