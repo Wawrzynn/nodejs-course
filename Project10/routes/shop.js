e78f5b54e823982611361d871ab99dac23cbf771
Wawrzynn <jedrzejkosakowski@gmail.com>
2023-09-26 13:51:15 +0200

    Project 10 changes added pug, ejs and handlebars.

--- Project10/routes/shop.js
+++ Project10/routes/shop.js
@@ -1,13 +1,12 @@
-const path = require('path');
-
-const express = require('express');
-
-const rootDir = require('../util/path');
-
+const path = require("path");
+const express = require("express");
+const rootDir = require("../util/path");
 const router = express.Router();
+const adminData = require("./admin");
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get("/", (req, res, next) => {
+  console.log(adminData.products);
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
