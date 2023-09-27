6fc1457ba1ee9cbca720b77bb96cd61766747dab
Wawrzynn <jedrzejkosakowski@gmail.com>
2023-09-27 11:39:01 +0200

    Finished project 10 added ejs solution. All three templating engines work.

--- Project10/app.js
+++ Project10/app.js
@@ -1,23 +1,27 @@
 const path = require("path");
-const hbs = require("express-handlebars");
+// const hbs = require("express-handlebars");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const ejs = require("ejs");
 
-app.engine(
-  "hbs",
-  hbs.engine({
-    extname: "hbs",
-    defaultLayout: "main-layout",
-    layoutsDir: "views/handlebars/layouts/",
-    extname: "hbs",
-  })
-);
+//registering handlebars view engine
+// app.engine(
+//   "hbs",
+//   hbs.engine({
+//     extname: "hbs",
+//     defaultLayout: "main-layout",
+//     layoutsDir: "views/handlebars/layouts/",
+//     extname: "hbs",
+//   })
+// );
 
 //app.set("view engine", "pug"); pug view engine
 //app.set("views", "views/pug/"); views for pug
-app.set("view engine", "hbs");
-app.set("views", "views/handlebars/");
+// app.set("view engine", "hbs"); handlebars view engine
+// app.set("views", "views/handlebars/"); views for handlebars
+app.set("view engine", "ejs"); //ejs view engine
+app.set("views", "views/ejs/"); //views for ejs
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -29,7 +33,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
