80e0cf4e12170bf1a29ce5796ec9b4315cdb05b2
Wawrzynn <jedrzejkosakowski@gmail.com>
2023-09-27 16:58:11 +0200

    Added project 14. Learning MVC design pattern.

---
+++ Project14/routes/login.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {registerView, loginView, registerUser, loginUser } = require('../controllers/loginController');
+const { protectRoute } = require("../auth/protect");
+const { dashboardView } = require("../controllers/dashboardController");
+
+const router = express.Router();
+
+router.get('/register', registerView);
+router.get('/login', loginView);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get("/dashboard", protectRoute, dashboardView);
+
+module.exports = router;
