ae75881806a790ff5d8827687bd362c1f9349b69
Wawrzynn <jedrzejkosakowski@gmail.com>
2023-10-02 16:19:37 +0200

    Finished project 16.

--- Project16/controllers/shop.js
+++ Project16/controllers/shop.js
@@ -33,3 +33,9 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: 'Checkout'
   });
 };
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+}
