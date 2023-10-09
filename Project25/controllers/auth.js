ef0aab0e904554dc19c088f709392e76a77aff5c
Wawrzyn <jedrzejkosakowski@gmail.com>
2023-10-09 12:56:18 +0200

    Finished project 25. Added cookies and sessions.

--- Project25/controllers/auth.js
+++ Project25/controllers/auth.js
@@ -1,14 +1,29 @@
-exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").trim().split("=")[1] === "true";
+const User = require('../models/user');
 
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
-  res.redirect("/");
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
