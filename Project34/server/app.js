c7480b8f801aa8bbb4d9754a482510f0df07da5f
Wawrzyn <jedrzejkosakowski@gmail.com>
2023-10-16 17:18:19 +0200

    Added project 34. Rest API async await refactor.

---
+++ Project34/server/app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const feedRoutes = require("./routes/feed");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const path = require("path");
+const multer = require("multer");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+dotenv.config();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/png" || "image/jpg" || "image/jpeg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use(
+  "/public/images",
+  express.static(path.join(__dirname, "public", "images"))
+);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message || "Internal server error.";
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then((result) => {
+    console.log("Connected to MongoDB.");
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
