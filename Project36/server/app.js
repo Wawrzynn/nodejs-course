346a8e1f68229c5550b8bf46035752297f14d407
Wawrzyn <jedrzejkosakowski@gmail.com>
2023-10-18 09:28:09 +0200

    Added image upload.

--- Project36/server/app.js
+++ Project36/server/app.js
@@ -1,10 +1,12 @@
 const path = require("path");
-const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const { graphqlHTTP } = require("express-graphql");
+const dotenv = require("dotenv");
+const fs = require("fs");
+
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
 const auth = require("./middleware/auth");
@@ -55,6 +57,21 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.use("/post-image", (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error("Not authenticated!");
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: "No file provided." });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: "File stored.", filePath: req.file.path });
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -87,3 +104,8 @@ mongoose
     app.listen(8080);
   })
   .catch((err) => console.log(err));
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
