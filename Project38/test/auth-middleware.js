319f909b0709d6bd65f2e592b657f6a0284d6b66
Wawrzyn <jedrzejkosakowski@gmail.com>
2023-10-21 18:47:30 +0200

    Added several controller and middleware test using chai, mocha and sinon.

---
+++ Project38/test/auth-middleware.js
@@ -0,0 +1,48 @@
+const authMiddleware = require("../middleware/is-auth");
+const expect = require("chai").expect;
+const jwt = require("jsonwebtoken");
+const sinon = require("sinon");
+
+describe("Auth middleware", () => {
+  it("should throw an error if no authorization header is present", function () {
+    const req = {
+      get: function (headerName) {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
+
+  it("should throw an error if the authorization header is only one string", function () {
+    const req = {
+      get: function (headerName) {
+        return "xyz";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should yield a userId after decoding the token", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer xyzshasdasdasdnasdmas";
+      },
+    };
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({ userId: "abc" });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId");
+    jwt.verify.restore();
+  });
+
+  it("should throw an error if the token cannot be verified", function () {
+    const req = {
+      get: function (headerName) {
+        return "Bearer xyz";
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+});
